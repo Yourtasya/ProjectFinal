651aaeb797b5a73054773455237f57973f8de4a7
Nastya <Nasteasmail6@gmail.com>
2023-07-02 23:59:39 +0300

    added inputs on homePage and Links on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,16 @@ import "../styles/Footer.css";
 import { Img } from "react-image";
 import { NavLink } from "react-router-dom";
 
-
 //@ts-ignore
 import LogoFooter from "../icons/Logo.svg";
+import {
+  ABOUT_ROUTE,
+  BLOG_ROUTE,
+  CONTACT_ROUTE,
+  HOME_ROUTE,
+  SERVICES_ROUTE,
+} from "../links/consts";
+import { NOT_FOUND_ROUTE } from "../links/ExceptionRoute/consts";
 
 export default class NavBar extends Component {
   render(): React.ReactNode {
@@ -23,14 +30,21 @@ export default class NavBar extends Component {
           <div className="footer_block">
             <div className="footer_block_1">
               <h3 className="footer_h3">Explore</h3>
-
-
+              <div className="footerNavLink">
+                <NavLink to={HOME_ROUTE}> Home</NavLink>
+                <NavLink to={ABOUT_ROUTE}> About Us</NavLink>
+                <NavLink to={SERVICES_ROUTE}> Services</NavLink>
+                <NavLink to={BLOG_ROUTE}> Blog</NavLink>
+                <NavLink to={HOME_ROUTE}> Home</NavLink>
+                <NavLink to={CONTACT_ROUTE}> Contact Us</NavLink>
+              </div>
             </div>
 
             <div className="footer_block_2">
               <h3 className="footer_h3">Utility Pages</h3>
-
-
+              <div className="footerNavLinkUtility">
+                <NavLink to={NOT_FOUND_ROUTE}>404 not found</NavLink>
+              </div>
             </div>
 
             <div className="footer_block_3">
@@ -38,33 +52,32 @@ export default class NavBar extends Component {
               <div className="footer_block_3_ul">
                 <div className="footer_block_3Adress">
                   <p className="footer_block_3_ulTittle">Address :</p>
-                  <p className="footer_block_3_ulTextAdress">24A Kingston St, Los Vegas NC 28202, USA.</p>
+                  <p className="footer_block_3_ulTextAdress">
+                    24A Kingston St, Los Vegas NC 28202, USA.
+                  </p>
                 </div>
 
                 <div className="footer_block_3Mail">
                   <p className="footer_block_3_ulTittle">Mail :</p>
-                  <p className="footer_block_3_ulTextMail">support@doctors.com</p>
+                  <p className="footer_block_3_ulTextMail">
+                    support@doctors.com
+                  </p>
                 </div>
 
                 <div className="footer_block_3Phone">
                   <p className="footer_block_3_ulTittle">Phone :</p>
-                  <p className="footer_block_3_ulTextPhone">(+22) 123 - 4567 - 900</p>
+                  <p className="footer_block_3_ulTextPhone">
+                    (+22) 123 - 4567 - 900
+                  </p>
                 </div>
-
               </div>
-
-
             </div>
 
             <div className="footer_block_4">
               <h3 className="footer_h3">Working Hours</h3>
-              <p className="workingHours">Mon to Fri: 7am - 6pm
-</p>
+              <p className="workingHours">Mon to Fri: 7am - 6pm</p>
               <p className="workingHours">Sat: 9am - 7pm</p>
               <p className="workingHours">Sun: 9am - 6pm</p>
-
-
-
             </div>
           </div>
         </div>
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -34,23 +34,13 @@ import Background2 from "../img/homePage/Background2.jpg";
 
 //svg
 //@ts-ignore
-import Book from "../icons/Book.svg";
-//@ts-ignore
-import Edit from "../icons/Edit.svg";
-//@ts-ignore
 import Icon from "../icons/Icon.svg";
 //@ts-ignore
 import Icon1 from "../icons/Icon(1).svg";
 //@ts-ignore
 import Icons3 from "../icons/Icons3.svg";
 //@ts-ignore
-import Mail from "../icons/Mail.svg";
-//@ts-ignore
-import Phone from "../icons/Phone.svg";
-//@ts-ignore
 import Reviews from "../icons/Reviews.svg";
-//@ts-ignore
-import User from "../icons/User.svg";
 //@ts-ignore
 import Vector1 from "../icons/Vector1.svg";
 //@ts-ignore
@@ -84,7 +74,6 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-
       <section className="HomePageProcedures">
         <div className="HomePageContainer">
           <div className="HomePageBlocks">
@@ -132,7 +121,6 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-
       <section className="HomePageAboutUs AboutUs">
         <div className="HomePageContainer ">
           <div className="AboutUsBlock">
@@ -151,7 +139,6 @@ export default function HomePage() {
 
                 <div className="leftBlock_cards">
                   <div className="leftBlock_card">
-
                     <div className="leftBlock_cardText">
                       <Img src={Icon}></Img>
                       <h3 className="leftBlock_cardTittle">Beauty Experts</h3>
@@ -180,7 +167,6 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-
       <section className="ourServicesBlock">
         <div className="ourServicesBlockContainer">
           <div className="ourServicesBlock_text">
@@ -267,13 +253,14 @@ export default function HomePage() {
                   The versions have evolved over the sometimes by accident.
                 </p>
               </div>
-              <button className="ourContentButton ourContentButton2">GET AN APPOINTMENT</button>
+              <button className="ourContentButton ourContentButton2">
+                GET AN APPOINTMENT
+              </button>
             </div>
             <Img className=".ourServicesFoto_2" src={Photo6}></Img>
           </div>
         </div>
       </section>
-
       <section className="TestimonialsBlock">
         <div className="ourServicesBlockContainer">
           <div className="TestimonialsBlock_text">
@@ -294,7 +281,7 @@ export default function HomePage() {
               </div>
               <div className="TestimonialFeedback_user">
                 <div className="TestimonialFeedback_userFoto">
-                <Img
+                  <Img
                     className="TestimonialFeedback_userFoto_1"
                     src={userPhoto1}
                   ></Img>
@@ -302,7 +289,6 @@ export default function HomePage() {
                 <div className="TestimonialFeedback_dataUser">
                   <div className="TestimonialFeedback_stars">
                     <Img src={Reviews}></Img>
-
                   </div>
                   <h3 className="TestimonialFeedback_userName">
                     James Williams
@@ -313,7 +299,6 @@ export default function HomePage() {
                 </div>
                 <div className="TestimonialFeedback_userElement">
                   <Img src={Icons3}></Img>
-
                 </div>
               </div>
             </div>
@@ -337,7 +322,6 @@ export default function HomePage() {
                 <div className="TestimonialFeedback_dataUser">
                   <div className="TestimonialFeedback_stars">
                     <Img src={Reviews}></Img>
-
                   </div>
                   <h3 className="TestimonialFeedback_userName">
                     James Williams
@@ -348,14 +332,12 @@ export default function HomePage() {
                 </div>
                 <div className="TestimonialFeedback_userElement">
                   <Img src={Icons3}></Img>
-
                 </div>
               </div>
             </div>
           </div>
         </div>
       </section>
-
       <section className="ourAwesome">
         <div className="HomePageContainer">
           <div className="ourAwesome_text">
@@ -368,76 +350,112 @@ export default function HomePage() {
             <div className="ourAwesome_product product">
               {/* <Img className="products_img" src={ }></Img> */}
               <div className="product_text">
-                <h3 className="product_textTittle product_textTittle_1">Optimised Energy Efficiency</h3>
-                <p className="product_textSubTittle">The point of using Lorem Ipsum is that  using making it look like readable.</p>
+                <h3 className="product_textTittle product_textTittle_1">
+                  Optimised Energy Efficiency
+                </h3>
+                <p className="product_textSubTittle">
+                  The point of using Lorem Ipsum is that using making it look
+                  like readable.
+                </p>
               </div>
             </div>
 
             <div className="ourAwesome_product product">
               {/* <Img className="products_img" src={ }></Img> */}
               <div className="product_text">
-                <h3 className="product_textTittle">Multi-faceted Water Filtration</h3>
-                <p className="product_textSubTittle">The point of using Lorem Ipsum is that  using making it look like readable.</p>
+                <h3 className="product_textTittle">
+                  Multi-faceted Water Filtration
+                </h3>
+                <p className="product_textSubTittle">
+                  The point of using Lorem Ipsum is that using making it look
+                  like readable.
+                </p>
               </div>
             </div>
 
             <div className="ourAwesome_product product">
               {/* <Img className="products_img" src={ }></Img> */}
               <div className="product_text">
-                <h3 className="product_textTittle">Uncompromising Quality and Durability</h3>
-                <p className="product_textSubTittle">The point of using Lorem Ipsum is that  using making it look like readable.</p>
+                <h3 className="product_textTittle">
+                  Uncompromising Quality and Durability
+                </h3>
+                <p className="product_textSubTittle">
+                  The point of using Lorem Ipsum is that using making it look
+                  like readable.
+                </p>
               </div>
             </div>
 
             <div className="ourAwesome_product product">
               {/* <Img className="products_img" src={ }></Img> */}
               <div className="product_text">
-                <h3 className="product_textTittle">An Ultra-Pampering Spa Experience</h3>
-                <p className="product_textSubTittle">The point of using Lorem Ipsum is that  using making it look like readable.</p>
+                <h3 className="product_textTittle">
+                  An Ultra-Pampering Spa Experience
+                </h3>
+                <p className="product_textSubTittle">
+                  The point of using Lorem Ipsum is that using making it look
+                  like readable.
+                </p>
               </div>
             </div>
 
             <div className="ourAwesome_product product">
               {/* <Img className="products_img" src={ }></Img> */}
               <div className="product_text">
-                <h3 className="product_textTittle">Easy Care for Unparalleled Enjoyment</h3>
-                <p className="product_textSubTittle">The point of using Lorem Ipsum is that  using making it look like readable.</p>
+                <h3 className="product_textTittle">
+                  Easy Care for Unparalleled Enjoyment
+                </h3>
+                <p className="product_textSubTittle">
+                  The point of using Lorem Ipsum is that using making it look
+                  like readable.
+                </p>
               </div>
             </div>
 
             <div className="ourAwesome_product product">
               {/* <Img className="products_img" src={ }></Img> */}
               <div className="product_text">
-                <h3 className="product_textTittle product_textTittle_1">Optimised Energy Efficiency</h3>
-                <p className="product_textSubTittle">The point of using Lorem Ipsum is that  using making it look like readable.</p>
+                <h3 className="product_textTittle product_textTittle_1">
+                  Optimised Energy Efficiency
+                </h3>
+                <p className="product_textSubTittle">
+                  The point of using Lorem Ipsum is that using making it look
+                  like readable.
+                </p>
               </div>
             </div>
           </div>
         </div>
-      </section>\
-
+      </section>
+      \
       <section className="flatDiscount">
         <div className="HomePageContainer">
           <div className="flatDiscountContent">
             <div className="flatDiscountText">
               <h4 className="flatDiscountText_title">Flat Discount </h4>
-              <h3 className="flatDiscountTex_subtittle">Claim upto 50% off
-                er
-                on ‍the most popular services...</h3>
-              <p className="flatDiscountText_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, buying to injected humour, or randomised words.</p>
+              <h3 className="flatDiscountTex_subtittle">
+                Claim upto 50% off er on ‍the most popular services...
+              </h3>
+              <p className="flatDiscountText_text">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, buying
+                to injected humour, or randomised words.
+              </p>
 
               <div className="flatDiscountText_flat flat">
                 <div className="flatRight">
                   <div className="flatRight_mailUs">
                     {/* <img src="" alt="" /> */}
-                    <p className="flatRight_mail">MAIL US :
-                      info@beautyness.com</p>
+                    <p className="flatRight_mail">
+                      MAIL US : info@beautyness.com
+                    </p>
                   </div>
 
                   <div className="flatRight_callUs">
                     {/* <img src="" alt="" /> */}
-                    <p className="flatRight_call">MAIL US :
-                      info@beautyness.com</p>
+                    <p className="flatRight_call">
+                      MAIL US : info@beautyness.com
+                    </p>
                   </div>
                 </div>
 
@@ -450,45 +468,59 @@ export default function HomePage() {
             <div className="flatDiscountTextForm">
               <div className="flatLeft_forms form">
                 <form action="" className="flatLeft_form">
-                  <label>
-                    <input type="text" name="name" id='username'
-                      className='input'
-                      placeholder='Ваше имя' />
-                  </label>
-                  <label>
-                    Имя:
-                    <input type="text" name="name" />
-                  </label>
-                  <label>
-                    Имя:
-                    <input type="text" name="name" />
-                  </label>
+                  <div className="wrapper">
+                    <div className="iconName"></div>
+                    <input placeholder="Name" className="input"></input>
+                  </div>
+                  <div className="wrapper">
+                    <div className="iconEmail"></div>
+                    <input placeholder="Email" className="input"></input>
+                  </div>
+                  <div className="wrapper">
+                    <div className="iconPhone"></div>
+                    <input placeholder="Phone" className="input"></input>
+                  </div>
+                  <div className="wrapper">
+                    <div className="iconService"></div>
+                    <input
+                      placeholder="Service You Need"
+                      className="input"
+                    ></input>
+                  </div>
+                  <div className="wrapper">
+                    <div className="iconAnyNote"></div>
+                    <input
+                      placeholder="Any Note For Us"
+                      className="input"
+                    ></input>
+                  </div>
                 </form>
               </div>
             </div>
           </div>
         </div>
       </section>
-
       <section className="ourBlog">
         <div className="HomePageContainer">
           <div className="ourBlog_text">
             <h4 className="ourBlog_tittle">Our Blog</h4>
-            <h2 className="ourBlog_subtittle">
-              Latest Updates
-            </h2>
+            <h2 className="ourBlog_subtittle">Latest Updates</h2>
           </div>
           <div className="ourBlogBlocks">
             <div className="ourBlock_1">
               <div className="ourBlockImg">
                 <Img src={Photo7}></Img>
-
               </div>
               <div className="Block_1_Text ourBlockText">
-                <h4 className="BlockText_ourBlog_text">11TH AUGUST 2021 - BY PETER BERGSON</h4>
-                <h3 className="BlockText_ourBlog_tittle ">The energy efficiency of the hydrotherapy or swim</h3>
+                <h4 className="BlockText_ourBlog_text">
+                  11TH AUGUST 2021 - BY PETER BERGSON
+                </h4>
+                <h3 className="BlockText_ourBlog_tittle ">
+                  The energy efficiency of the hydrotherapy or swim
+                </h3>
                 <p className="BlockText_ourBlog_subtittle">
-                  The point of using Lorem Ipsum hiter that using making it look like others readable will get end.
+                  The point of using Lorem Ipsum hiter that using making it look
+                  like others readable will get end.
                 </p>
               </div>
             </div>
@@ -496,14 +528,18 @@ export default function HomePage() {
             <div className="ourBlock_2">
               <div className="BlockImg">
                 <Img src={Photo8}></Img>
-
               </div>
               <div className="Block_2_Text ourBlockText">
-                <h4 className="BlockText_ourBlog_text">11TH AUGUST 2021 - BY PETER BERGSON</h4>
+                <h4 className="BlockText_ourBlog_text">
+                  11TH AUGUST 2021 - BY PETER BERGSON
+                </h4>
 
-                <h3 className="BlockText_ourBlog_tittle">Tips to improve your body via cleansing</h3>
+                <h3 className="BlockText_ourBlog_tittle">
+                  Tips to improve your body via cleansing
+                </h3>
                 <p className="BlockText_ourBlog_subtittle">
-                  The point of using Lorem Ipsum hiter that using making it look like others readable will get end.
+                  The point of using Lorem Ipsum hiter that using making it look
+                  like others readable will get end.
                 </p>
               </div>
             </div>
@@ -513,11 +549,16 @@ export default function HomePage() {
                 <Img src={Photo9}></Img>
               </div>
               <div className="Block_3_Text ourBlockText">
-                <h4 className="BlockText_ourBlog_text">11TH AUGUST 2021 - BY PETER BERGSON</h4>
+                <h4 className="BlockText_ourBlog_text">
+                  11TH AUGUST 2021 - BY PETER BERGSON
+                </h4>
 
-                <h3 className="BlockText_ourBlog_tittle">Experience the beauty at our beauty spa</h3>
+                <h3 className="BlockText_ourBlog_tittle">
+                  Experience the beauty at our beauty spa
+                </h3>
                 <p className="BlockText_ourBlog_subtittle">
-                  The point of using Lorem Ipsum hiter that using making it look like others readable will get end.
+                  The point of using Lorem Ipsum hiter that using making it look
+                  like others readable will get end.
                 </p>
               </div>
             </div>
@@ -527,15 +568,9 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-
       <section>
         <Img src={Background4}></Img>
-        <Img src={Book}></Img>
-        <Img src={Edit}></Img>
         <Img src={Icons3}></Img>
-        <Img src={Mail}></Img>
-        <Img src={Phone}></Img>
-        <Img src={User}></Img>
         <Img src={Vector2}></Img>
         <Img src={Background2}></Img>
       </section>
